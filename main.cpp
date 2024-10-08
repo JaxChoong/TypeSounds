e86d4d7ed09607fc75f72fc3dfc35513758fe582
Jax Choong <choongjax@gmail.com>
2024-10-08 15:06:11 +0800

    feat: now detects keypresses and plays sound whenever keyboard is pressed

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <Windows.h>
 #include <mmsystem.h>
+#include <conio.h>
 #include <iostream>
 #include <string>
 
 #pragma comment(lib,"winmm.lib")
 
 int main(){
-    std::cout << "Playing Music \n";
-    PlaySound(TEXT("megalovania.wav"),NULL,SND_FILENAME |SND_ASYNC | SND_LOOP);
-    Sleep(5000);
+    std::cout << "Press any key! \n";
+    while (true){
+        if (_kbhit()){
+            int key = _getch();  // Get the pressed key (non-blocking)
+            std::cout << key ;
+            // Check if the Escape key is pressed to stop the sound
+            if (key == 27) {  // 27 is ASCII code for Escape key
+                PlaySound(NULL, 0, 0);  // Stop any playing sound
+                std::cout << "Sound stopped. Exiting...\n";
+                break;  // Exit the loop
+            }
+            PlaySound(TEXT("megalovania.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+        }
+    }
+    Sleep(100);
     return 0;
 }
