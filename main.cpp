ac809b93b7a77a638323c3e3633e93b86ae665a7
Jax Choong <choongjax@gmail.com>
2024-10-09 21:03:19 +0800

    feat: now can play actual randomized audios

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <conio.h>  // for kbhit and getch
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 #pragma comment(lib,"winmm.lib")
 
@@ -18,7 +20,17 @@ int main(){
                 std::cout << "Sound stopped. Exiting...\n";
                 break;  // Exit the loop
             }
-            PlaySound(TEXT("megalovania.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+            srand(time(0));  // generate seed for randomiser
+            int audioNum = rand() % 18; // get number between 0 and 17
+            audioNum++;  // +1 to get between 1 and 18
+            std::cout << "this is audioNum" +  std::to_string(audioNum) + "heh\n";
+            std::string audioFile = "sounds/" + std::to_string(audioNum) + ".wav";   // generate file name
+            std::cout << audioFile;
+            const char* audioFileCStr = audioFile.c_str(); // get acceptable string format for TEXT()
+            PlaySound(audioFileCStr, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+            if (GetLastError() != 0) {
+                std::cout << "Error playing sound: " << GetLastError() << std::endl;
+            }
         }
     }
     Sleep(100);
