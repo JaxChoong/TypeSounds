6b67692f2f9640f3bde8cd3d59144b9936b06b7c
Jax Choong <choongjax@gmail.com>
2024-10-11 14:43:29 +0800

    feat: now can swap between profiles with "tab"

--- main.cpp
+++ main.cpp
@@ -5,27 +5,44 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 #pragma comment(lib,"winmm.lib")
 
 
+std::vector<std::string> soundProfiles = {"AC/", "Another/"};
+int soundProfileIndex = 0;
+
 int main(){
     std::cout << "Press any key! \n";
-    srand(static_cast<unsigned int>(time(0)));                                       // generate seed for randomizer
+    srand(static_cast<unsigned int>(time(0)));      
+    std::string soundDirectory = "sounds/";                                 
+    std::string soundProfilePath = "AC/";                                 // start with animal crossing sound profile
     while (true){
         if (_kbhit()){
             int key = _getch();                                                      // Get the pressed key (non-blocking)
             // Check if the Escape key is pressed to stop the sound
-            std::cout << char(key);
+            std::cout << "key" << key<< std::endl;
             if (key == 27) {                                                         // 27 is ASCII code for Escape key
                 PlaySound(NULL, 0, 0);                                               // Stop any playing sound
                 std::cout << "Sound stopped. Exiting...\n";
                 break;                                                               // Exit the loop
             }
+            if (key == 9){  // tab key
+                try{                                                                // try to swap to the next sound profile
+                    soundProfileIndex++;
+                    soundProfilePath = soundProfiles.at(soundProfileIndex);
+                }
+                catch(std::out_of_range){                                           // if out of range, go back to the first sound profile
+                    soundProfileIndex = 0;
+                    soundProfilePath = soundProfiles.at(soundProfileIndex);
+                }
+            }
             PlaySound(NULL, 0, 0); 
             int audioNum = rand() % 18;                                              // get number between 0 and 17
             audioNum++;                                                              // +1 to get between 1 and 18
-            std::string audioFile = "sounds/AC/" + std::to_string(audioNum) + ".wav";   // generate file name
+            std::cout << std::endl << soundDirectory + soundProfilePath;
+            std::string audioFile = soundDirectory + soundProfilePath + std::to_string(audioNum) + ".wav";   // generate file name
 
             PlaySound(audioFile.c_str(), NULL, SND_FILENAME | SND_ASYNC);
         }
